3ee72212beb323e92259416c4eb723fbf04c7470
OxBonaparte <>
2022-11-19 11:30:27 +0100

    fix: build problem again

--- pages/api/[address].ts
+++ pages/api/[address].ts
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
-import whitelistsData from "./whitelistsdata.json";
+import whitelistsData from "./whitelistsData.json";
 
 type Data = {
   address?: string;
