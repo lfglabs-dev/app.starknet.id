98b29ded4e90abebda02dc3a92ccdbd1b88f04ab
Thomas Marchand <thomas.marchand@tuta.io>
2022-11-27 14:15:17 +0100

    feat: add analytics component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,7 @@ import Head from "next/head";
 import { ThemeProvider } from "@mui/material";
 import theme from "../styles/theme";
 import { InjectedConnector, StarknetProvider } from "@starknet-react/core";
+import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }: AppProps) {
   const connectors = [
@@ -23,6 +24,7 @@ function MyApp({ Component, pageProps }: AppProps) {
           <Component {...pageProps} />
         </ThemeProvider>
       </StarknetProvider>
+      <Analytics />
     </>
   );
 }
