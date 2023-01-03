8b4cd30ec50624b9f75fa5253d38e3aff599ead0
OxBonaparte <>
2023-01-03 11:00:01 +0100

    fix: add only one const

--- tests/hooks/encoding.test.js
+++ tests/hooks/encoding.test.js
@@ -1,15 +1,15 @@
 /* eslint-disable no-undef */
 import { BN } from "bn.js";
 import { useDecoded, useEncoded } from "../../hooks/naming";
-import { generateString } from "../../utils/stringService";
-
-const charactersWithBigAlphabet = "abcdefghijklmnopqrstuvwxyz0123456789-这来";
+import {
+  charactersWithBigAlphabet,
+  generateString,
+} from "../../utils/stringService";
 
 describe("Should test encoding/decoding hooks 2500 times", () => {
   it("Should test useEncoded and useDecoded hook with a random string", () => {
     for (let index = 0; index < 2500; index++) {
       const randomString = generateString(10, charactersWithBigAlphabet);
-      // eslint-disable-next-line react-hooks/rules-of-hooks
       expect(useDecoded([useEncoded(randomString)])).toBe(
         randomString + ".stark"
       );
@@ -20,10 +20,8 @@ describe("Should test encoding/decoding hooks 2500 times", () => {
 describe("Should test decoding/encoding hooks 2500 times", () => {
   it("Should test useDecoded and useEncoded hook with a number", () => {
     for (let index = 0; index < 2500; index++) {
-      // eslint-disable-next-line react-hooks/rules-of-hooks
       const decoded = useDecoded([new BN(index)]);
       expect(
-        // eslint-disable-next-line react-hooks/rules-of-hooks
         useEncoded(decoded.substring(0, decoded.length - 6)).toString()
       ).toBe(index.toString());
     }
