850e2619d6650983653832ef60d64189b04efede
Thomas Marchand <thomas.marchand@tuta.io>
2023-01-12 13:48:44 +0100

    feat: add id_to_infts

---
+++ pages/api/indexer/id_to_infts.ts
@@ -0,0 +1,36 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { connectToDatabase } from "../../../lib/mongodb";
+import NextCors from "nextjs-cors";
+import { QueryError } from "../../../types/backTypes";
+
+type IdentityNFT = {
+  contract: string,
+  inft_id: string,
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Array<IdentityNFT> | QueryError>
+) {
+  await NextCors(req, res, {
+    methods: ["GET"],
+    origin: "*",
+    optionsSuccessStatus: 200,
+  });
+  const {
+    query: { id },
+  } = req;
+  const { db } = await connectToDatabase();
+  const domainCollection = db.collection("equipped_infts");
+  const output: Array<IdentityNFT> = [];
+  await domainCollection
+    .find({ starknet_id: id, "_chain.valid_to": null })
+    .forEach((doc) => {
+      output.push({ contract: doc.contract, inft_id: doc.inft_id });
+    });
+
+  res
+    .setHeader("cache-control", "max-age=30")
+    .status(200)
+    .json(output);
+}
