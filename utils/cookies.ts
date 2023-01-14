3c85ade449a4c40cbb1cade11fe2db9a46f02dc2
nicolasito1411 <60229704+Marchand-Nicolas@users.noreply.github.com>
2023-01-14 09:30:17 +0100

    Replacing some let by const

--- utils/cookies.ts
+++ utils/cookies.ts
@@ -1,14 +1,14 @@
 export function setCookie(cname:string, cvalue:string, exdays:any) {
   const d = new Date();
   d.setTime(d.getTime() + (exdays*24*60*60*1000));
-  let expires = "expires="+ d.toUTCString();
+  const expires = "expires="+ d.toUTCString();
   document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
 
 export function getCookie(cname:string) {
-  let name = cname + "=";
-  let decodedCookie = decodeURIComponent(document.cookie);
-  let ca = decodedCookie.split(';');
+  const name = cname + "=";
+  const decodedCookie = decodeURIComponent(document.cookie);
+  const ca = decodedCookie.split(';');
   for(let i = 0; i <ca.length; i++) {
     let c = ca[i];
     while (c.charAt(0) == ' ') {
