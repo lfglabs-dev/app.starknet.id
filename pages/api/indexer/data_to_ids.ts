e72d13026fbd528eb3a14a4a91a789c2ff51a9e3
Thomas Marchand <thomas.marchand@tuta.io>
2023-01-22 17:33:55 +0100

    feat: add data_to_ids endpoint to reverse find a starknet_id from a data

---
+++ pages/api/indexer/data_to_ids.ts
@@ -0,0 +1,40 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { connectToDatabase } from "../../../lib/mongodb";
+import NextCors from "nextjs-cors";
+import { QueryError } from "../../../types/backTypes";
+
+type Data = {
+  starknet_id: string;
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data | QueryError>
+) {
+  await NextCors(req, res, {
+    methods: ["GET"],
+    origin: "*",
+    optionsSuccessStatus: 200,
+  });
+  const {
+    query: { verifier, field, data },
+  } = req;
+  const { db } = await connectToDatabase();
+
+  await db.collection("starknet_ids_data")
+    .findOne({ verifier, field, data, "_chain.valid_to": null })
+    .then((doc) => {
+      if (doc) {
+        res
+          .setHeader("cache-control", "max-age=30")
+          .status(200)
+          .json({ starknet_id: doc.token_id });
+      } else {
+        res
+          .setHeader("cache-control", "max-age=30")
+          .status(200)
+          .json({ error: "no tokenid associated to this data was found" });
+      }
+    });
+
+}
