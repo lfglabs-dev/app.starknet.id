2283d4f8adf856cd08ab28108f0492ec8a8267ac
Paul-Henry Kajfasz <kajfaszph@gmail.com>
2023-03-03 18:24:32 -0700

    improvements

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,32 +5,26 @@ import styles2 from "../styles/domain.module.css";
 import SearchBar from "../components/UI/searchBar";
 
 const Domain: NextPage = () => {
-  return (
-    <div className={styles.screen}>
-      <div className={styles.firstLeaf}>
-        <img alt="leaf" src="/leaves/leaf_2.png" />
-      </div>
-      <div className={styles.secondLeaf}>
-        <img alt="leaf" src="/leaves/leaf_1.png" />
-      </div>
-      <div className={styles2.containerSearch}>
-        <div className={styles2.searchBarContainer}>
-          <div className="flex flex-col items-center">
-            <img
-              src="/visuals/StarknetIdLogo.svg"
-              height={250}
-              width={250}
-              alt="logo"
-            />
-          </div>
-          <h1 className="title">Choose your .stark domain</h1>
-          <div className="min-w-content mx-5 mt-5 flex justify-center align-center">
-            <SearchBar />
-          </div>
+    return (
+        <div className={styles.screen}>
+            <div className={styles2.containerSearch}>
+                <div className={styles2.searchBarContainer}>
+                    <div className="flex flex-col items-center">
+                        <img
+                            src="/visuals/StarknetIdLogo.svg"
+                            height={250}
+                            width={250}
+                            alt="logo"
+                        />
+                    </div>
+                    <h1 className="title">Choose your .stark domain</h1>
+                    <div className="min-w-content mx-5 mt-5 flex justify-center align-center">
+                        <SearchBar />
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Domain;
