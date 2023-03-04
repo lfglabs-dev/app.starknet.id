947b15a9195e940e97beca331c7a2e145530fdb7
Paul-Henry Kajfasz <kajfaszph@gmail.com>
2023-03-03 18:24:32 -0700

    improvements

--- pages/search.tsx
+++ pages/search.tsx
@@ -12,70 +12,64 @@ import { is1234Domain, isStarkRootDomain } from "../utils/stringService";
 import { useAccount } from "@starknet-react/core";
 
 const SearchPage: NextPage = () => {
-  const router = useRouter();
-  const [isMenuVisible, setIsMenuVisible] = useState<boolean>(false);
-  const [domain, setDomain] = useState<string>("");
-  const { account } = useAccount();
-  const [isAvailable, setIsAvailable] = useState<boolean | undefined>(
-    undefined
-  );
-  const { expiry: data, error } = useExpiryFromDomain(domain);
+    const router = useRouter();
+    const [isMenuVisible, setIsMenuVisible] = useState<boolean>(false);
+    const [domain, setDomain] = useState<string>("");
+    const { account } = useAccount();
+    const [isAvailable, setIsAvailable] = useState<boolean | undefined>(
+        undefined
+    );
+    const { expiry: data, error } = useExpiryFromDomain(domain);
 
-  useEffect(() => {
-    if (
-      router?.query?.domain &&
-      isStarkRootDomain(router.query.domain.concat(".stark") as string)
-    ) {
-      setDomain(router.query.domain as string);
-    }
-  }, [router]);
+    useEffect(() => {
+        if (
+            router?.query?.domain &&
+            isStarkRootDomain(router.query.domain.concat(".stark") as string)
+        ) {
+            setDomain(router.query.domain as string);
+        }
+    }, [router]);
 
-  useEffect(() => {
-    const currentTimeStamp = new Date().getTime() / 1000;
+    useEffect(() => {
+        const currentTimeStamp = new Date().getTime() / 1000;
 
-    if (error || !data || is1234Domain(domain)) {
-      setIsAvailable(false);
-    } else {
-      setIsAvailable(Number(data?.["expiry"]) < currentTimeStamp);
-    }
-  }, [data, error, domain]);
+        if (error || !data || is1234Domain(domain)) {
+            setIsAvailable(false);
+        } else {
+            setIsAvailable(Number(data?.["expiry"]) < currentTimeStamp);
+        }
+    }, [data, error, domain]);
 
-  useEffect(() => {
-    if (account) {
-      setIsMenuVisible(true);
-    } else {
-      setIsMenuVisible(false);
-    }
-  }, [isAvailable, account]);
+    useEffect(() => {
+        if (account) {
+            setIsMenuVisible(true);
+        } else {
+            setIsMenuVisible(false);
+        }
+    }, [isAvailable, account]);
 
-  return (
-    <div className={styles.screen}>
-      <div className={styles.firstLeaf}>
-        <img width="100%" alt="leaf" src="/leaves/leaf_2.png" />
-      </div>
-      <div className={styles.secondLeaf}>
-        <img width="100%" alt="leaf" src="/leaves/leaf_1.png" />
-      </div>
-      <div className={styles2.container}>
-        <div className="sm:w-2/3 w-4/5 mt-5">
-          <SearchBar
-            onChangeTypedValue={(typeValue: string) => setDomain(typeValue)}
-          />
-          {domain && (
-            <DomainCard
-              isAvailable={isAvailable}
-              domain={domain.concat(".stark")}
-              isConnected={Boolean(account)}
-            />
-          )}
+    return (
+        <div className={styles.screen}>
+            <div className={styles2.container}>
+                <div className="sm:w-2/3 w-4/5 mt-5">
+                    <SearchBar
+                        onChangeTypedValue={(typeValue: string) => setDomain(typeValue)}
+                    />
+                    {domain && (
+                        <DomainCard
+                            isAvailable={isAvailable}
+                            domain={domain.concat(".stark")}
+                            isConnected={Boolean(account)}
+                        />
+                    )}
+                </div>
+
+                {isMenuVisible ? (
+                    <DomainMenu isAvailable={isAvailable} domain={domain as string} />
+                ) : null}
+            </div>
         </div>
-
-        {isMenuVisible ? (
-          <DomainMenu isAvailable={isAvailable} domain={domain as string} />
-        ) : null}
-      </div>
-    </div>
-  );
+    );
 };
 
 export default SearchPage;
