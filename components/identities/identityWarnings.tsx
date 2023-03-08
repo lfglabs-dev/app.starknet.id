365aa7cf870191e747abb6b106ebd5f667cb7f87
Fricoben <benjamin.flores.pro@gmail.com>
2023-03-08 17:02:22 +0100

    ref: add warning component for the tokenId

--- components/identities/identityWarnings.tsx
+++ components/identities/identityWarnings.tsx
@@ -1,6 +1,9 @@
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useEffect, useState } from "react";
 import { Identity } from "../../types/backTypes";
 import { getDomainWithoutStark, isSubdomain } from "../../utils/stringService";
+import { Alert, Snackbar } from "@mui/material";
+import { hexToDecimal } from "../../utils/feltService";
+import { useAccount } from "@starknet-react/core";
 import Link from "next/link";
 
 type IdentityWarningsProps = {
@@ -10,25 +13,47 @@ type IdentityWarningsProps = {
 const IdentityWarnings: FunctionComponent<IdentityWarningsProps> = ({
   identity,
 }) => {
+  const { address } = useAccount();
   const currentTimeStamp = new Date().getTime() / 1000;
+  const isExpired =
+    Number(identity?.domain_expiry) < currentTimeStamp &&
+    !isSubdomain(identity?.domain);
+  const isDifferentAddress = identity?.addr != hexToDecimal(address);
+  const [showWarning, setShowWarning] = useState(false);
+
+  useEffect(() => {
+    if ((isExpired || isDifferentAddress) && address) {
+      setShowWarning(true);
+    }
+  }, [isDifferentAddress, isExpired, address]);
 
   return (
-    <>
-      {Number(identity?.domain_expiry) < currentTimeStamp &&
-      !isSubdomain(identity?.domain) ? (
-        <strong className="mt-2 text-red-600 text-center">
-          (This domain has expired you can buy it on the&nbsp;
-          <span className="underline">
-            <Link
-              href={"/search?domain=" + getDomainWithoutStark(identity?.domain)}
-            >
-              domain page
-            </Link>
-          </span>
-          )
-        </strong>
-      ) : null}
-    </>
+    <Snackbar
+      anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+      open={showWarning}
+    >
+      <Alert severity="error" onClose={() => setShowWarning(false)}>
+        {isExpired && (
+          <>
+            This domain has expired you can buy it on the&nbsp;
+            <span className="underline">
+              <Link
+                href={
+                  "/search?domain=" + getDomainWithoutStark(identity?.domain)
+                }
+              >
+                domain page
+              </Link>
+            </span>
+          </>
+        )}
+        {isDifferentAddress && (
+          <>
+            &nbsp;Be careful this domain is not linked to your current address.
+          </>
+        )}
+      </Alert>
+    </Snackbar>
   );
 };
 
