2f665dae922ebee2790b7e2e3a370ccefb27538b
Fricoben <benjamin.flores.pro@gmail.com>
2023-04-19 17:10:33 +0200

    wip: add web wallet

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,18 +5,20 @@ import Navbar from "../components/UI/navbar";
 import Head from "next/head";
 import { ThemeProvider } from "@mui/material";
 import theme from "../styles/theme";
-import { InjectedConnector, StarknetProvider } from "@starknet-react/core";
+import { InjectedConnector, StarknetConfig } from "@starknet-react/core";
+import { WebWalletConnector } from "@argent/starknet-react-webwallet-connector";
 import { Analytics } from "@vercel/analytics/react";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const connectors = [
     new InjectedConnector({ options: { id: "argentX" } }),
     new InjectedConnector({ options: { id: "braavos" } }),
+    new WebWalletConnector({ url: "https://web.hydrogen.argent47.net" }),
   ];
 
   return (
     <>
-      <StarknetProvider connectors={connectors}>
+      <StarknetConfig autoConnect connectors={connectors}>
         <ThemeProvider theme={theme}>
           <Head>
             <title>Starknet.id</title>
@@ -29,7 +31,7 @@ function MyApp({ Component, pageProps }: AppProps) {
           <Component {...pageProps} />
         </ThemeProvider>
         <Analytics />
-      </StarknetProvider>
+      </StarknetConfig>
     </>
   );
 }
