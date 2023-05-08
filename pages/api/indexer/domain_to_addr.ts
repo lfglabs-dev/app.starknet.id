d52b2e41bc02e2e8c4e99f8c79b9b2bb9a675b85
Iris <iris.devillars@gmail.com>
2023-05-08 11:11:35 -0700

    feat: add subdomains to domain_to_addr endpoint

--- pages/api/indexer/domain_to_addr.ts
+++ pages/api/indexer/domain_to_addr.ts
@@ -1,8 +1,9 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { connectToDatabase } from "../../../lib/mongodb";
 import NextCors from "nextjs-cors";
+import { utils } from "starknetid.js";
 
-type DomainToAddrData = { addr: string; domain_expiry: number };
+type DomainToAddrData = { addr: string; domain_expiry?: number };
 
 export default async function handler(
   req: NextApiRequest,
@@ -17,20 +18,41 @@ export default async function handler(
     query: { domain },
   } = req;
   const { db } = await connectToDatabase();
-  await db
-    .collection("domains")
-    .findOne({
-      domain: domain,
-      "_chain.valid_to": null,
-    })
-    .then((domainDoc) => {
-      res
-        .setHeader("cache-control", "max-age=60")
-        .status(200)
-        .json(
-          domainDoc
-            ? { addr: domainDoc.addr, domain_expiry: domainDoc.expiry }
-            : { error: "no address found" }
-        );
-    });
+  if (
+    typeof domain === "string" &&
+    // todo: add utils function for Xplorer subdomains
+    (utils.isBraavosSubdomain(domain) || domain.endsWith(".Xplorer.stark"))
+  ) {
+    await db
+      .collection("subdomains")
+      .findOne({
+        domain: domain,
+        "_chain.valid_to": null,
+      })
+      .then((domainDoc) => {
+        res
+          .setHeader("cache-control", "max-age=60")
+          .status(200)
+          .json(
+            domainDoc ? { addr: domainDoc.addr } : { error: "no address found" }
+          );
+      });
+  } else {
+    await db
+      .collection("domains")
+      .findOne({
+        domain: domain,
+        "_chain.valid_to": null,
+      })
+      .then((domainDoc) => {
+        res
+          .setHeader("cache-control", "max-age=60")
+          .status(200)
+          .json(
+            domainDoc
+              ? { addr: domainDoc.addr, domain_expiry: domainDoc.expiry }
+              : { error: "no address found" }
+          );
+      });
+  }
 }
