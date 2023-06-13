237b8fc1602d7aa91ca00d25892cd0609dbe0fc0
Fricoben <78437165+fricoben@users.noreply.github.com>
2023-06-13 15:12:34 +0200

    fix: twitter api (#265)

--- components/identities/actions/clickable/clickableTwitterIcon.tsx
+++ components/identities/actions/clickable/clickableTwitterIcon.tsx
@@ -9,7 +9,6 @@ import React, {
 import TwitterIcon from "../../../UI/iconsComponents/icons/twitterIcon";
 import VerifiedIcon from "../../../UI/iconsComponents/icons/verifiedIcon";
 import styles from "../../../../styles/components/icons.module.css";
-import { minifyDomain } from "../../../../utils/stringService";
 import { StarknetIdJsContext } from "../../../../context/StarknetIdJsProvider";
 
 type ClickableTwitterIconProps = {
@@ -27,7 +26,6 @@ const ClickableTwitterIcon: FunctionComponent<ClickableTwitterIconProps> = ({
 }) => {
   const router = useRouter();
   const [twitterId, setTwitterId] = useState<string | undefined>();
-  const [twitterUsername, setTwitterUsername] = useState<string | undefined>();
   const { starknetIdNavigator } = useContext(StarknetIdJsContext);
 
   useEffect(() => {
@@ -36,34 +34,25 @@ const ClickableTwitterIcon: FunctionComponent<ClickableTwitterIconProps> = ({
       .then((response) => {
         if (response.toString(10) !== "0") {
           setTwitterId(response.toString(10));
+        } else {
+          setTwitterId(undefined);
         }
       })
       .catch(() => {
         return;
       });
-  }, []);
+  }, [tokenId, domain]);
 
   function startVerification(link: string): void {
     sessionStorage.setItem("tokenId", tokenId);
     router.push(link);
   }
 
-  useEffect(() => {
-    if (twitterId) {
-      fetch(`/api/twitter/get_username?id=${twitterId}`)
-        .then((response) => response.json())
-        // TO DO : Find how to import the twitter response type
-        .then((data) => {
-          setTwitterUsername(data[0].username);
-        });
-    }
-  }, [twitterId]);
-
   return isOwner ? (
     <Tooltip
       title={
-        twitterUsername
-          ? `Change your twitter account from ${twitterUsername} to another one`
+        twitterId
+          ? "Change your twitter account on Starknet ID"
           : "Start twitter verification"
       }
       arrow
@@ -76,20 +65,11 @@ const ClickableTwitterIcon: FunctionComponent<ClickableTwitterIconProps> = ({
           )
         }
       >
-        {twitterUsername ? (
+        {twitterId ? (
           <div className={styles.verifiedIcon}>
-            <VerifiedIcon width={width} color={"green"} />
+            <VerifiedIcon width={"18"} color={"green"} />
           </div>
         ) : null}
-        <TwitterIcon width={width} color={"white"} />
-      </div>
-    </Tooltip>
-  ) : twitterUsername ? (
-    <Tooltip title={`Check ${minifyDomain(domain)} twitter`} arrow>
-      <div
-        className={styles.clickableIconTwitter}
-        onClick={() => window.open(`https://twitter.com/${twitterUsername}`)}
-      >
         <TwitterIcon width={width} color={"white"} />
       </div>
     </Tooltip>
