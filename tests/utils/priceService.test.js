56a6e34f20c2962b9614d2cfeebe802a29267f77
Fricoben <benjamin.flores.pro@gmail.com>
2023-09-04 16:48:53 +0200

    fix: register problems

--- tests/utils/priceService.test.js
+++ tests/utils/priceService.test.js
@@ -1,35 +1,37 @@
 /* eslint-disable no-undef */
 import { totalAlphabet } from "../../utils/constants";
-import { PRICES, getYearlyPriceFromDomain } from "../../utils/priceService";
+import { PRICES, getPriceFromDomain } from "../../utils/priceService";
 import { generateString } from "../../utils/stringService";
 
 describe("Should test price service file", () => {
-  it("Test getYearlyPriceFromDomain functions with different domains", () => {
+  it("Test getPriceFromDomain functions with different domains", () => {
     let randomString = generateString(0, totalAlphabet);
-    expect(getYearlyPriceFromDomain(randomString.concat(".stark"))).toEqual(
-      PRICES.ONE
+    expect(getPriceFromDomain(1, randomString.concat(".stark"))).toEqual(
+      PRICES.ONE.toString()
     );
 
     randomString = generateString(1, totalAlphabet);
-    expect(getYearlyPriceFromDomain(randomString.concat(".stark"))).toEqual(
-      PRICES.ONE
+    expect(getPriceFromDomain(1, randomString.concat(".stark"))).toEqual(
+      PRICES.ONE.toString()
     );
 
     randomString = generateString(2, totalAlphabet);
-    expect(getYearlyPriceFromDomain(randomString.concat(".stark"))).toEqual(
-      PRICES.TWO
+    expect(getPriceFromDomain(1, randomString.concat(".stark"))).toEqual(
+      PRICES.TWO.toString()
     );
 
     randomString = generateString(3, totalAlphabet);
-    expect(getYearlyPriceFromDomain(randomString)).toEqual(PRICES.THREE);
+    expect(getPriceFromDomain(1, randomString)).toEqual(
+      PRICES.THREE.toString()
+    );
 
     randomString = generateString(4, totalAlphabet);
-    expect(getYearlyPriceFromDomain(randomString)).toEqual(PRICES.FOUR);
+    expect(getPriceFromDomain(1, randomString)).toEqual(PRICES.FOUR.toString());
 
     randomString = generateString(5, totalAlphabet);
-    expect(getYearlyPriceFromDomain(randomString)).toEqual(PRICES.FIVE);
+    expect(getPriceFromDomain(1, randomString)).toEqual(PRICES.FIVE.toString());
 
     randomString = generateString(6, totalAlphabet);
-    expect(getYearlyPriceFromDomain(randomString)).toEqual(PRICES.FIVE);
+    expect(getPriceFromDomain(1, randomString)).toEqual(PRICES.FIVE.toString());
   });
 });
