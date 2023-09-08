702815daa814cfd68bf96e6faf16d77eff12fac3
Fricoben <78437165+fricoben@users.noreply.github.com>
2023-09-08 12:55:34 +0200

    fix: mail check (#384)

--- utils/stringService.ts
+++ utils/stringService.ts
@@ -152,7 +152,10 @@ export function cleanUsername(username: string): string {
 
 export function isValidEmail(email: string): boolean {
   if (email.includes("..")) return false; // Ensure no consecutive dots
-  return /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/.test(email);
+  const emailRegex =
+    // eslint-disable-next-line no-control-regex
+    /^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$/;
+  return emailRegex.test(email);
 }
 
 export function isValidDomain(domain: string | undefined): boolean | string {
