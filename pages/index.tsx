d5112ff8c62755f3c522471cfcc4ab85d1c11af1
Thomas Marchand <thomas.marchand@tuta.io>
2023-09-11 10:50:37 +0100

    feat: improve catch phrase

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,8 +13,8 @@ const Domain: NextPage = () => {
             <div className="flex flex-col justify-start items-start text-center sm:text-start">
               <h1 className="title">Choose your Stark Domain</h1>
               <p className="description">
-                Your unified profile across the starknet ecosystem, one name for
-                all your Starknet on-chain identity.
+                Be unmistakable on Starknet: One profile, seamlessly connecting
+                you to the entire ecosystem.
               </p>
             </div>
             <SearchBar showHistory />
