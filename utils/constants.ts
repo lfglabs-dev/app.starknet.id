6cc79560526376660ea03dd38b1a7c0dd0d005ad
Iris <iris.devillars@gmail.com>
2023-11-13 17:23:54 +0300

    fix: build error

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,7 +1,7 @@
 export const basicAlphabet = "abcdefghijklmnopqrstuvwxyz0123456789-";
 export const bigAlphabet = "这来";
 export const totalAlphabet = basicAlphabet + bigAlphabet;
-export const UINT_128_MAX = (BigInt(1) << BigInt(128)) - BigInt(1);‡
+export const UINT_128_MAX = (BigInt(1) << BigInt(128)) - BigInt(1);
 
 export const PFP_WL_CONTRACTS_TESTNET = [
   "0x041e1382e604688da7f22e7fbb6113ba3649b84a87b58f4dc1cf5bfa96dfc2cf",
