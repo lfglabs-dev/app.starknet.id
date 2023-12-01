325c53952ebd701e6b144e9aaba3fbe1be51ee03
Thomas Marchand <thomas.marchand@tuta.io>
2023-12-01 11:40:40 +0000

    Revert "fix: CDN url"
    This reverts commit 0fa8270ef1f7cdef0a133db571329c3439036e2b.

--- next.config.js
+++ next.config.js
@@ -8,7 +8,11 @@ module.exports = {
     };
   },
   reactStrictMode: true,
-  assetPrefix: isProd ? process.env.NEXT_PUBLIC_CDN_URL : undefined,
+  assetPrefix: isProd
+    ? process.env.NEXT_PUBLIC_IS_TESTNET === "true"
+      ? "https://goerli.cdn.starknet.quest"
+      : "https://cdn.starknet.quest"
+    : undefined,
   webpack: (config, { isServer }) => {
     if (!isServer) {
       config.resolve.fallback.fs = false;
