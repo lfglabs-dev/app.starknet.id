39765882ee2cc1fefad367105f51cbd98e7b8a1d
Fricoben <78437165+fricoben@users.noreply.github.com>
2023-12-20 11:36:01 +0000

    fix: modal confirm (#592)

--- components/identities/actions/subdomainModal.tsx
+++ components/identities/actions/subdomainModal.tsx
@@ -3,7 +3,6 @@ import { useAccount, useContractWrite } from "@starknet-react/core";
 import React, { FunctionComponent, useEffect, useState } from "react";
 import { useIsValid } from "../../../hooks/naming";
 import styles from "../../../styles/components/modalMessage.module.css";
-import { hexToDecimal } from "../../../utils/feltService";
 import { numberToString } from "../../../utils/stringService";
 import SelectIdentity from "../../domains/selectIdentity";
 import Button from "../../UI/button";
@@ -103,11 +102,16 @@ const SubdomainModal: FunctionComponent<SubdomainModalProps> = ({
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [transferDomainData]); // We want to call this only once when the transaction is sent
 
+  function closeModal(): void {
+    setIsTxSent(false);
+    handleClose();
+  }
+
   return (
     <Modal
       disableAutoFocus
       open={isModalOpen}
-      onClose={handleClose}
+      onClose={closeModal}
       aria-labelledby="modal-modal-title"
       aria-describedby="modal-modal-description"
     >
