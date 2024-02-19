a89c115c495db03b86acd53fcbc5e90322f0502b
Iris <iris.devillars@gmail.com>
2024-02-19 11:30:55 +0100

    fix: okx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -30,7 +30,9 @@ if (typeof window !== "undefined") {
 export const availableConnectors = [
   new InjectedConnector({ options: { id: "braavos", name: "Braavos" } }),
   new InjectedConnector({ options: { id: "argentX", name: "Argent X" } }),
-  new InjectedConnector({ options: { id: "okxwallet", name: "OKX" } }),
+  new InjectedConnector({
+    options: { id: "okxwallet", name: "OKX" },
+  }),
   new WebWalletConnector({
     url:
       process.env.NEXT_PUBLIC_IS_TESTNET === "true"
