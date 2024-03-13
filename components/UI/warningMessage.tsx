56a998ac605810e52ace1721057865c6ce5a212a
nicolasito1411 <60229704+Marchand-Nicolas@users.noreply.github.com>
2024-03-13 14:09:53 +0100

    feat: no NFT on pfp settings

---
+++ components/UI/warningMessage.tsx
@@ -0,0 +1,17 @@
+import { FunctionComponent } from "react";
+
+type WarningMessageProps = {
+  children: React.ReactNode;
+};
+
+const WarningMessage: FunctionComponent<WarningMessageProps> = ({
+  children,
+}) => {
+  return (
+    <div className="flex items-center justify-center bg-[#FFE6CC] px-4 py-2 rounded-lg">
+      <p className=" text-base text-[#F57C00] font-bold">{children}</p>
+    </div>
+  );
+};
+
+export default WarningMessage;
