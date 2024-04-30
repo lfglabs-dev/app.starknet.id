9543192a07d733bb424cd17a2606256d662f80de
Iris <iris.devillars@gmail.com>
2024-04-30 13:37:12 +0200

    fix: smartCurrencyChoosing firing too early

--- components/domains/steps/checkoutCard.tsx
+++ components/domains/steps/checkoutCard.tsx
@@ -67,7 +67,9 @@ const CheckoutCard: FunctionComponent<CheckoutCardProps> = ({
   groups,
 }) => {
   const router = useRouter();
-  const { account, address } = useAccount();
+  const address =
+    "0x061b6c0a78f9edf13cea17b50719f3344533fadd470b8cb29c2b4318014f52d3";
+  const { account } = useAccount();
   const { formState, updateFormState, clearForm } = useContext(FormContext);
   const [priceInEth, setPriceInEth] = useState<string>(""); // price in ETH for 1 year
   const [price, setPrice] = useState<string>(""); // total price in displayedCurrency, set to priceInEth on first load as ETH is the default currency
@@ -177,7 +179,11 @@ const CheckoutCard: FunctionComponent<CheckoutCardProps> = ({
 
   // we choose the currency based on the user balances
   useEffect(() => {
-    if (tokenBalances && !hasChosenCurrency) {
+    if (
+      tokenBalances &&
+      Object.keys(tokenBalances).length > 0 &&
+      !hasChosenCurrency
+    ) {
       smartCurrencyChoosing(tokenBalances).then((currency) => {
         onCurrencySwitch(currency);
         setHasChosenCurrency(true);
