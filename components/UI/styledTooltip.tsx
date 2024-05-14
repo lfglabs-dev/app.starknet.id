691f065645ad61ab5db66a0d05124280b0e3c91e
nicolasito1411 <60229704+Marchand-Nicolas@users.noreply.github.com>
2024-05-14 19:09:53 +0200

    fix: tooltips mobile touch

--- components/UI/styledTooltip.tsx
+++ components/UI/styledTooltip.tsx
@@ -2,7 +2,12 @@ import React from "react";
 import { Tooltip, TooltipProps, styled, tooltipClasses } from "@mui/material";
 
 const StyledToolTip = styled(({ className, ...props }: TooltipProps) => (
-  <Tooltip {...props} arrow classes={{ popper: className }} />
+  <Tooltip
+    {...props}
+    arrow
+    classes={{ popper: className }}
+    enterTouchDelay={0}
+  />
 ))(() => ({
   [`& .${tooltipClasses.arrow}`]: {
     color: "#454545",
