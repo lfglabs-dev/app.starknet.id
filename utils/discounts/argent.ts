8b78bf844b63d11893f2045ea30ee243bc2b9762
nicolasito1411 <60229704+Marchand-Nicolas@users.noreply.github.com>
2024-06-17 15:46:37 +0200

    updating discount id and timestamp

--- utils/discounts/argent.ts
+++ utils/discounts/argent.ts
@@ -2,7 +2,7 @@ export const argentDiscount = {
   offer: {
     duration: 81, // in days
     customMessage: "3 months (with Argent discount)",
-    discountId: "1",
+    discountId: "0x617267656e745f6f6e655f646f6c6c6172",
     price: "599178082191783",
     desc: "Secure your domain for the next 3 months at a special discounted price. Don't miss out on this exclusive opportunity to buy your domain for only 1$!",
     title: { desc: "Get your domain", catch: "for 1$" },
@@ -10,7 +10,7 @@ export const argentDiscount = {
   },
   name: "Argent 1$ domain",
   image: "/quantumleap/quantumLeapAstro.webp",
-  expiry: 1720216800 * 1000, // timestamp in ms
+  expiry: 1719874800 * 1000, // timestamp in ms
   discountMailGroupId: "98859014745490932",
   sponsor: "argent.stark",
 };
