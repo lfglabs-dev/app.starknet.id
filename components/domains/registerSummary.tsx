5a5cb0a5251fe2279326f5e4947d693e7b5a84e9
Fricoben <benjamin.flores.pro@gmail.com>
2024-06-24 11:45:49 +0100

    fix: summary again

--- components/domains/registerSummary.tsx
+++ components/domains/registerSummary.tsx
@@ -116,9 +116,6 @@ const RegisterSummary: FunctionComponent<RegisterSummaryProps> = ({
   }
 
   function displayTokenPrice(): ReactNode {
-    if (!ethRegistrationPrice || !salesTaxRate || !registrationPrice)
-      return null;
-
     const salesTaxAmountUsd =
       salesTaxRate *
       Number(gweiToEth(ethRegistrationPrice)) *
