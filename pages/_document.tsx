81cee57b3442dee1fc12263225f7a8cf7b472ec0
nicolasito1411 <60229704+Marchand-Nicolas@users.noreply.github.com>
2024-08-19 16:18:02 +0200

    fix: twitter preview image not loading

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -19,7 +19,10 @@ export default function Document() {
           content="Starknet.id allows you to create your starknet passport. Create a virgin identity or link your existing one to a .stark domain to building your Starknet Reputation !"
         />
         <meta property="og:url" content={process.env.NEXT_PUBLIC_APP_LINK} />
-        <meta property="og:image" content="/visuals/starknetIdMeta.webp" />
+        <meta
+          property="og:image"
+          content={`${process.env.NEXT_PUBLIC_APP_LINK}/visuals/starknetIdMeta.webp`}
+        />
         <meta
           name="description"
           content="Starknet.id allows you to create your starknet passport. Create a virgin identity or link your existing one to a .stark domain to building your Starknet Reputation !"
@@ -33,7 +36,10 @@ export default function Document() {
           name="twitter:description"
           content="Starknet.id allows you to create your starknet passport. Create a virgin identity or link your existing one to a .stark domain to building your Starknet Reputation !"
         />
-        <meta name="twitter:image" content="/visuals/starknetIdMeta.webp" />
+        <meta
+          name="twitter:image"
+          content={`${process.env.NEXT_PUBLIC_APP_LINK}/visuals/starknetIdMeta.webp`}
+        />
       </Head>
 
       <body className="default_background_color">
