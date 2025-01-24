fdf14a5b3b113cf06d02e1bc681cacf3cb5463f1
code-stan <thecoderstanley@gmail.com>
2025-01-24 09:01:00 +0100

    modified the input structure and input

--- components/UI/textField.tsx
+++ components/UI/textField.tsx
@@ -5,63 +5,61 @@ import { TextField as TextFieldMui } from "@mui/material";
 import InputHelper from "./inputHelper";
 
 type TextFieldProps = {
-  label: string;
-  value: string;
-  onChange: OutlinedInputProps["onChange"];
-  color: "primary" | "secondary" | "error" | "info" | "success" | "warning";
-  required?: boolean;
-  error?: boolean;
-  errorMessage?: string;
-  placeholder?: string;
-  helperText?: string;
-  type?: React.InputHTMLAttributes<unknown>["type"];
-  variant?: "default" | "white";
+	label: string;
+	value: string;
+	onChange: OutlinedInputProps["onChange"];
+	color: "primary" | "secondary" | "error" | "info" | "success" | "warning";
+	required?: boolean;
+	error?: boolean;
+	errorMessage?: string;
+	placeholder?: string;
+	helperText?: string;
+	type?: React.InputHTMLAttributes<unknown>["type"];
+	variant?: "default" | "white";
+	centerLabel?: true | false;
 };
 
-const TextField: FunctionComponent<TextFieldProps> = ({
-  label,
-  value,
-  onChange,
-  color,
-  required = false,
-  error = false,
-  errorMessage,
-  placeholder,
-  helperText,
-  type,
-  variant = "default",
-}) => {
-  return (
-    <div className="flex flex-col w-full">
-      <div className="flex gap-1 my-1">
-        {error ? (
-          <p className={styles.errorLegend}>{errorMessage}</p>
-        ) : (
-          <p className={styles.legend}>{label}</p>
-        )}
-        {required ? "*" : ""}
-      </div>
-      <InputHelper helperText={helperText} error={error}>
-        <TextFieldMui
-          type={type}
-          error={error}
-          fullWidth
-          value={value}
-          variant="outlined"
-          onChange={onChange}
-          color={color}
-          InputProps={{
-            classes: {
-              root: styles.textfield,
-              input: variant === "white" ? styles.inputWhite : "",
-            },
-          }}
-          required={required}
-          placeholder={placeholder}
-        />
-      </InputHelper>
-    </div>
-  );
+const TextField: FunctionComponent<TextFieldProps> = ({ label, value, onChange, color, required = false, error = false, errorMessage, placeholder, helperText, type, variant = "default", centerLabel = false }) => {
+	return (
+		<div className="flex flex-col w-full">
+			<div className={`flex gap-1 mb-[9.8px] ${centerLabel && "justify-center"}`}>
+				{error ? <p className={styles.errorLegend}>{errorMessage}</p> : <p className={styles.legend}>{label}</p>}
+				{required ? "*" : ""}
+			</div>
+			<InputHelper helperText={helperText} error={error}>
+				<TextFieldMui
+					type={type}
+					error={error}
+					fullWidth
+					value={value}
+					variant="outlined"
+					onChange={onChange}
+					color={color}
+					InputProps={{
+						classes: {
+							root: styles.textfield,
+							input: variant === "white" ? styles.inputWhite : "",
+						},
+					}}
+					sx={{
+						"& .MuiOutlinedInput-root": {
+							"boxShadow": "0px 2px 30px 0px #0000000F", // Adds shadow
+              "height": "56px",
+              "background-color": "#FFFFFF",
+							"& fieldset": {
+								borderColor: "#CDCCCC",
+							},
+							"&:hover fieldset": {
+								borderColor: "#CDCCDD",
+							},
+						},
+					}}
+					required={required}
+					placeholder={placeholder}
+				/>
+			</InputHelper>
+		</div>
+	);
 };
 
 export default TextField;
