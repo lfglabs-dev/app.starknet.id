2eb36e5c646205d02bf92f8590f4b644c8efa253
code-stan <thecoderstanley@gmail.com>
2025-01-25 13:31:33 +0100

    removed conflicting styles

--- pages/newsletter.tsx
+++ pages/newsletter.tsx
@@ -45,10 +45,10 @@ const NewsletterPage: NextPage = () => {
         <Image src="/visuals/coconut.webp" alt="coconut" fill />
       </div>
       <div className={styles.tree1}>
-        <Image src="/visuals/coconut-tree-1.png" alt="coconut" fill />
+        <Image src="/visuals/coconutTree1.webp" alt="coconut tree" fill />
       </div>
       <div className={styles.tree2}>
-        <Image src="/visuals/coconut-tree-2.png" alt="coconut" fill />
+        <Image src="/visuals/coconutTree2.webp" alt="coconut tree" fill />
       </div>
       <p className={styles.subtitle}>Newsletter</p>
       <h1 className={styles.title}>
