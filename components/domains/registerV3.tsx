31fcb589b4ff2028b4ea012d3016d8adb2cd1197
viktohblake <damanygt@gmail.com>
2025-01-25 18:06:24 +0100

    Fix: Steps for purchasing a domain #950

--- components/domains/registerV3.tsx
+++ components/domains/registerV3.tsx
@@ -5,9 +5,9 @@ import UserInfoForm from "./steps/userInfoForm";
 import { FormType } from "@/utils/constants";
 import CheckoutCard from "./steps/checkoutCard";
 import { useAccount } from "@starknet-react/core";
+import styles from "../../styles/components/registerV3.module.css";
 import SelectPfp from "./steps/selectPfp";
 import RegisterSteps from "./steps/registerSteps";
-import SearchBar from "../UI/searchBar";
 import evergreenDiscounts from "@/utils/discounts/evergreen";
 
 type RegisterV3Props = {
@@ -17,7 +17,7 @@ type RegisterV3Props = {
 
 const RegisterV3: FunctionComponent<RegisterV3Props> = ({
   domain,
-  setDomain,
+  setDomain, // Not used since domain search bar is removed for this component
 }) => {
   const { address } = useAccount();
   const [currentStep, setCurrentStep] = useState(1);
@@ -57,35 +57,52 @@ const RegisterV3: FunctionComponent<RegisterV3Props> = ({
 
   return (
     <>
-      {currentStep > 1 ? (
-        <RegisterSteps
-          currentStep={currentStep}
-          setStep={goToStep}
-          showPfp={userNfts && userNfts.length > 0}
-          isLoading={isLoadingNfts}
-        />
-      ) : (
-        <div className="sm:w-2/5 w-4/5 mt-5 mb-5">
-          <SearchBar
-            onChangeTypedValue={(typeValue: string) => setDomain(typeValue)}
-            showHistory={false}
+      <div className="flex flex-col md:flex-row gap-10">
+        <aside className={styles.purchaseStepNav}>
+          <RegisterSteps
+            currentStep={currentStep}
+            setStep={goToStep}
+            showPfp={userNfts && userNfts.length > 0}
+            isLoading={isLoadingNfts}
           />
+
+    <img 
+      src="/visuals/purchaseStepVisual.svg" 
+    />
+        </aside>
+
+        <div className={styles.purchaseStepNavMobile}>
+          <RegisterSteps
+            currentStep={currentStep}
+            setStep={goToStep}
+            showPfp={userNfts && userNfts.length > 0}
+            isLoading={isLoadingNfts}
+          />
+
+  <div className="flex justify-center">
+    <img 
+      src="/visuals/purchaseStepVisualMobile.svg" 
+    />
+  </div>
         </div>
-      )}
-      {currentStep === 1 && (
-        <UserInfoForm
-          type={FormType.REGISTER}
-          goToNextStep={goToNextStep}
-          imageUrl="/visuals/register.webp"
-        />
-      )}
-      {currentStep === 2 && <SelectPfp goToNextStep={goToNextStep} />}
-      {currentStep === 3 && (
-        <CheckoutCard
-          type={FormType.REGISTER}
-          discount={evergreenDiscounts.registration}
-        />
-      )}
+
+        <div className="flex-1">
+          {currentStep === 1 && (
+            <UserInfoForm
+              type={FormType.REGISTER}
+              goToNextStep={goToNextStep}
+              imageUrl="/visuals/register.webp"
+            />
+          )}
+          {currentStep === 2 && <SelectPfp goToNextStep={goToNextStep} />}
+          {currentStep === 3 && (
+            <CheckoutCard
+              type={FormType.REGISTER}
+              discount={evergreenDiscounts.registration}
+            />
+          )}
+        </div>
+      </div>
     </>
   );
 };
--- components/domains/steps/registerSteps.tsx
+++ components/domains/steps/registerSteps.tsx
@@ -5,12 +5,25 @@ import PfpIcon from "@/components/UI/iconsComponents/icons/pfpIcon";
 import CartIcon from "@/components/UI/iconsComponents/icons/cartIcon";
 import theme from "@/styles/theme";
 import { Skeleton } from "@mui/material";
+import DoneFilledIcon from "@/components/UI/iconsComponents/icons/doneFilledIcon";
 
 type registerStepsProps = {
   currentStep: number;
   setStep: (step: number) => void;
   showPfp?: boolean;
   isLoading?: boolean;
+};
+
+const getStep = (currentStep: number, stepIndex: number) => {
+  if (currentStep > stepIndex) return styles.passedStep;
+  if (currentStep === stepIndex) return styles.activeStep;
+  return styles.disabledStep;
+};
+
+const getStepColor = (currentStep: number, step: number): string => {
+  if (currentStep > step) return theme.palette.primary.main;
+  if (currentStep === step) return theme.palette.secondary.main;
+  return theme.palette.grey[200];
 };
 
 const RegisterSteps: FunctionComponent<registerStepsProps> = ({
@@ -21,85 +34,84 @@ const RegisterSteps: FunctionComponent<registerStepsProps> = ({
 }) => {
   return isLoading ? (
     <div className={styles.stepsContainer}>
-      <div className={styles.progressSteps}>
-        <Skeleton variant="circular" width="35px" height="35px" />
-        <Skeleton variant="rounded" width="60px" height="2px" />
-        <Skeleton variant="circular" width="35px" height="35px" />
-        <Skeleton variant="rounded" width="60px" height="2px" />
-        <Skeleton variant="circular" width="35px" height="35px" />
-      </div>
+<div className="flex gap-2 items-center">
+  <Skeleton variant="circular" width={24} height={24} />
+  <div className="w-12 sm:w-16 md:w-24 lg:w-36">
+    <Skeleton variant="text" height={20} />
+  </div>
+</div>
+<div className="flex gap-2 items-center">
+  <Skeleton variant="circular" width={24} height={24} />
+  <div className="w-12 sm:w-16 md:w-24 lg:w-36">
+    <Skeleton variant="text" height={20} />
+  </div>
+</div>
+<div className="flex gap-2 items-center">
+  <Skeleton variant="circular" width={24} height={24} />
+  <div className="w-12 sm:w-16 md:w-24 lg:w-36">
+    <Skeleton variant="text" height={20} />
+  </div>
+</div>
+
     </div>
   ) : (
     <div className={styles.stepsContainer}>
-      <div className={styles.progressSteps}>
-        <div
-          className={`${styles.progressStep} ${
-            currentStep >= 1 ? styles.activeStep : ""
-          }`}
-          onClick={() => setStep(1)}
-        >
+      <div
+        className={`${styles.step} ${getStep(currentStep, 1)}`}
+        onClick={() => currentStep >= 1 && setStep(1)}
+      >
+        <div className={styles.stepContent}>
           <ContactCardIcon
-            width="30"
-            color={
-              currentStep >= 1
-                ? theme.palette.primary.main
-                : theme.palette.secondary.main
-            }
+            width="20"
+            color={getStepColor(currentStep, 1)}
           />
-          <p>Domain</p>
+          <p className={styles.stepText}>Domain</p>
         </div>
+        {currentStep > 1 && (
+          <DoneFilledIcon
+            width="16"
+            secondColor={theme.palette.primary.main}
+            color="#FFF"
+          />
+        )}
+      </div>
+
+      {showPfp && (
         <div
-          className={`${styles.progressLine} ${
-            currentStep >= 2 ? styles.primaryBg : ""
-          }`}
-        />
-        {showPfp ? (
-          <>
-            <div
-              className={`${styles.progressStep} ${
-                currentStep >= 2 ? styles.activeStep : ""
-              }`}
-              onClick={() => setStep(2)}
-            >
-              <PfpIcon
-                width="30"
-                secondColor={
-                  currentStep >= 2 ? "#FFF" : theme.palette.secondary.main
-                }
-                color={
-                  currentStep >= 2
-                    ? theme.palette.primary.main
-                    : theme.palette.secondary.light
-                }
-              />
-              <p>PFP</p>
-            </div>
-            <div
-              className={`${styles.progressLine} ${
-                currentStep >= 3 ? styles.primaryBg : ""
-              }`}
-            />
-          </>
-        ) : null}
-        <div
-          className={`${styles.progressStep} ${
-            currentStep >= 3 ? styles.activeStep : ""
-          }`}
-          onClick={() => setStep(3)}
+          className={`${styles.step} ${getStep(currentStep, 2)}`}
+          onClick={() => currentStep >= 2 && setStep(2)}
         >
-          <CartIcon
-            width="30"
-            secondColor={
-              currentStep >= 3 ? "#FFF" : theme.palette.secondary.main
-            }
-            color={
-              currentStep >= 3
-                ? theme.palette.primary.main
-                : theme.palette.secondary.light
-            }
-          />
-          <p>Checkout</p>
+          <div className={styles.stepContent}>
+            <PfpIcon
+              width="20"
+              color={getStepColor(currentStep, 2)}
+            />
+            <p className={styles.stepText}>PFP</p>
+          </div>
+          {currentStep > 2 && (
+            <DoneFilledIcon
+              width="16"
+              secondColor={theme.palette.primary.main}
+              color="#FFF"
+            />
+          )}
         </div>
+      )}
+
+      <div
+        className={`${styles.step} ${getStep(currentStep, 3)}`}
+        onClick={() => currentStep >= 3 && setStep(3)}
+      >
+        <div className={styles.stepContent}>
+          <CartIcon
+            width="20"
+            color={getStepColor(currentStep, 3)}
+          />
+          <p className={styles.stepText}>Checkout</p>
+        </div>
+        {currentStep > 3 && (
+          <DoneFilledIcon width="16" color={theme.palette.primary.main} />
+        )}
       </div>
     </div>
   );
