2270ab89e11bfac49e09d148538a217ba937a0f6
Nico <60229704+Marchand-Nicolas@users.noreply.github.com>
2025-02-01 11:48:07 +0100

    ref: cleaning the code

--- pages/identities/[tokenId].tsx
+++ pages/identities/[tokenId].tsx
@@ -1,20 +1,19 @@
 import React from "react";
 import AvailableIdentities from "@/components/identities/availableIdentities";
 import homeStyles from "../../styles/Home.module.css";
-import {NextPage} from "next";
-import {useRouter} from "next/router";
-import BackButton from "@/components/UI/backButton";
+import { NextPage } from "next";
+import { useRouter } from "next/router";
 
 const TokenIdPage: NextPage = () => {
-   const router = useRouter();
-   const tokenId: string = router.query.tokenId as string;
-   return (
-      <div className={homeStyles.wrapperScreen}>
-         <div className="mt-[8rem]">
-            <AvailableIdentities tokenId={tokenId} />
-         </div>
+  const router = useRouter();
+  const tokenId: string = router.query.tokenId as string;
+  return (
+    <div className={homeStyles.wrapperScreen}>
+      <div className="mt-[8rem]">
+        <AvailableIdentities tokenId={tokenId} />
       </div>
-   );
+    </div>
+  );
 };
 
 export default TokenIdPage;
