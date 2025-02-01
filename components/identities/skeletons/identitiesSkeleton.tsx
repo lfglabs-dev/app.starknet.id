861d62131fc3a3cb9e46ecac93cffaf8340364fe
Nico <60229704+Marchand-Nicolas@users.noreply.github.com>
2025-02-01 11:52:33 +0100

    ref: cleaning the code

--- components/identities/skeletons/identitiesSkeleton.tsx
+++ components/identities/skeletons/identitiesSkeleton.tsx
@@ -1,17 +1,27 @@
-import {Skeleton} from "@mui/material";
-import React, {FunctionComponent} from "react";
+import { Skeleton } from "@mui/material";
+import React, { FunctionComponent } from "react";
 import styles from "../../../styles/components/identitiesV1.module.css";
 
 const IdentitiesSkeleton: FunctionComponent = () => {
-   return (
-      <div className="">
-         <div className={styles.identitiesSkeleton}>
-            <Skeleton variant="rounded" width={300} height={546} className=""/>
-            <Skeleton variant="rounded" width={598} height={346} className="mt-20 hidden md:flex"/>
-            <Skeleton variant="rounded" width={398} height={246} className="mt-32 hidden lg:flex"/>
-         </div>
+  return (
+    <div className="">
+      <div className={styles.identitiesSkeleton}>
+        <Skeleton variant="rounded" width={300} height={546} className="" />
+        <Skeleton
+          variant="rounded"
+          width={598}
+          height={346}
+          className="mt-20 hidden md:flex"
+        />
+        <Skeleton
+          variant="rounded"
+          width={398}
+          height={246}
+          className="mt-32 hidden lg:flex"
+        />
       </div>
-   );
+    </div>
+  );
 };
 
 export default IdentitiesSkeleton;
