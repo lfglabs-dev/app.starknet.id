da884bf234da272c313c3cdccd46299b364218f6
Nico <60229704+Marchand-Nicolas@users.noreply.github.com>
2025-02-11 19:16:47 +0100

    fix discount id

--- utils/discounts/argent.ts
+++ utils/discounts/argent.ts
@@ -2,7 +2,7 @@ export const argentDiscount = {
   offer: {
     duration: 365,
     customMessage: "-50% (with Argent discount)",
-    discountId: "3",
+    discountId: "2",
     price: "4509315068492700",
     desc: "Get your domain for half the price. Don't miss out on this exclusive opportunity only available with your Argent wallet!",
     title: { desc: "Mint your domain", catch: "-50% discount" },
