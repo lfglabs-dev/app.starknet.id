f493d578bb790fe6e584652c78c7eabf3379cbdd
pheobeayo <pheobeayo@gmail>
2025-02-14 14:57:11 +0100

    fix build issues

--- components/UI/inputHelper.tsx
+++ components/UI/inputHelper.tsx
@@ -1,12 +1,26 @@
 import React, { FunctionComponent, ReactNode } from "react";
+import StyledToolTip from "./styledTooltip";
 
 type InputHelperProps = {
   children: ReactNode;
+  helperText?: string;
   error?: boolean;
 };
 
-const InputHelper: FunctionComponent<InputHelperProps> = ({ children, error = false }) => {
-  return <>{children}</>;
+const InputHelper: FunctionComponent<InputHelperProps> = ({
+  children,
+  helperText,
+}) => {
+  return (
+    <div className="relative">
+      {children}
+      {helperText && (
+        <StyledToolTip className="cursor-pointer" title={helperText} placement="top" >
+          <> </>
+        </StyledToolTip>
+      )}
+    </div>
+  );
 };
 
 export default InputHelper;
