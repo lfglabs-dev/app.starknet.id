413f499ef9332ba738ed46594cfdf7e784fe8530
Nico <60229704+Marchand-Nicolas@users.noreply.github.com>
2025-02-15 22:48:35 +0100

    fix container height

--- components/domains/renewalV2.tsx
+++ components/domains/renewalV2.tsx
@@ -11,7 +11,6 @@ import SelectPfp from "./steps/selectPfp";
 import { StarknetIdJsContext } from "@/context/StarknetIdJsProvider";
 import evergreenDiscounts from "@/utils/discounts/evergreen";
 
-
 const RenewalV2: FunctionComponent = () => {
   const { address } = useAccount();
   const [currentStep, setCurrentStep] = useState(1);
@@ -74,58 +73,54 @@ const RenewalV2: FunctionComponent = () => {
     }
   };
 
-return (
-  <>
-    <div className="w-full flex flex-col xl:flex-row justify-center gap-4 px-3 py-4 lg:px-32 md:px-16 sm:py-12 xl:h-[calc(100vh-6rem)]">
-      <aside className={`${styles.purchaseStepNav}`} role="navigation">
-       <RegisterSteps
-        currentStep={currentStep}
-        setStep={goToStep}
-        showPfp={showPfp}
-        isLoading={isLoading}
-      />
+  return (
+    <>
+      <div className="w-full flex flex-col xl:flex-row justify-center gap-4 px-3 py-4 lg:px-32 md:px-16 sm:py-12 xl:h-[88vh]">
+        <aside className={`${styles.purchaseStepNav}`} role="navigation">
+          <RegisterSteps
+            currentStep={currentStep}
+            setStep={goToStep}
+            showPfp={showPfp}
+            isLoading={isLoading}
+          />
 
-        <img
-          src="/visuals/purchaseStepVisual.svg"
-          alt="Domain purchase steps visualization"
-        />
-      </aside>
-
-      <div className={`${styles.purchaseStepNavMobile}`} role="navigation">
-        <RegisterSteps
-        currentStep={currentStep}
-        setStep={goToStep}
-        showPfp={showPfp}
-        isLoading={isLoading}
-      />
-
-        <div className="flex justify-center">
           <img
-            src="/visuals/purchaseStepVisualMobile.svg"
+            src="/visuals/purchaseStepVisual.svg"
             alt="Domain purchase steps visualization"
           />
+        </aside>
+
+        <div className={`${styles.purchaseStepNavMobile}`} role="navigation">
+          <RegisterSteps
+            currentStep={currentStep}
+            setStep={goToStep}
+            showPfp={showPfp}
+            isLoading={isLoading}
+          />
+
+          <div className="flex justify-center">
+            <img
+              src="/visuals/purchaseStepVisualMobile.svg"
+              alt="Domain purchase steps visualization"
+            />
+          </div>
+        </div>
+
+        <div className="flex-1">
+          {currentStep === 1 && (
+            <UserInfoForm type={FormType.RENEW} goToNextStep={goToNextStep} />
+          )}
+          {currentStep === 2 && <SelectPfp goToNextStep={goToNextStep} />}
+          {currentStep === 3 && (
+            <CheckoutCard
+              type={FormType.RENEW}
+              discount={evergreenDiscounts.renewal}
+            />
+          )}
         </div>
       </div>
-
-      
-      <div className="flex-1">
-        {currentStep === 1 && (
-          <UserInfoForm
-            type={FormType.RENEW}
-            goToNextStep={goToNextStep}
-          />
-        )}
-        {currentStep === 2 && <SelectPfp goToNextStep={goToNextStep} />}
-        {currentStep === 3 && (
-        <CheckoutCard
-          type={FormType.RENEW}
-          discount={evergreenDiscounts.renewal}
-        />
-      )}
-      </div>
-    </div>
-  </>
-);
+    </>
+  );
 };
 
 export default RenewalV2;
