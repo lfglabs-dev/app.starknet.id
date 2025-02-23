9335cd30381333c080a58aaff19a645a2dce3fa9
Emmanex01 <nwanosiketochukwu99@gmail.com>
2025-02-23 23:42:12 +0100

    changed Cancel button style

--- components/identities/updateProfilePic.tsx
+++ components/identities/updateProfilePic.tsx
@@ -65,7 +65,7 @@ const UpdateProfilePic: FunctionComponent<UpdateProfilePicProps> = ({
                 </div>
                 <div className="flex justify-center mt-4">
                   <button 
-                    className={styles.cancelBtn}
+                    className={styles.cancelButton}
                     onClick={back}
                   >
                     CANCEL
