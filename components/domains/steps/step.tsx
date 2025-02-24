556be166098ba166d6ac31570cc758aed364323e
JayWebtech <jethroadamzy@gmail.com>
2025-02-24 15:55:36 +0100

    refac : reviewed fixes

---
+++ components/domains/steps/step.tsx
@@ -0,0 +1,65 @@
+import React from "react";
+import styles from "../../../styles/components/registerV3.module.css";
+import DoneFilledIcon from "@/components/UI/iconsComponents/icons/doneFilledIcon";
+import theme from "@/styles/theme";
+
+type StepProps = {
+  stepIndex: number;
+  currentStep: number;
+  setStep: (step: number) => void;
+  icon: React.ReactNode;
+  label: string;
+  showDoneIcon?: boolean;
+  allowSwitchAnytime?: boolean;
+};
+
+const Step: React.FC<StepProps> = ({
+  stepIndex,
+  currentStep,
+  setStep,
+  icon,
+  label,
+  showDoneIcon = true,
+  allowSwitchAnytime = false,
+}) => {
+  const getStep = (currentStep: number, stepIndex: number): string => {
+    if (currentStep > stepIndex)
+      return allowSwitchAnytime ? "" : styles.passedStep;
+    if (currentStep === stepIndex) return styles.activeStep;
+    return allowSwitchAnytime ? "" : styles.disabledStep;
+  };
+
+  const getStepColor = (currentStep: number, step: number): string => {
+    if (currentStep > step)
+      return allowSwitchAnytime ? "" : theme.palette.primary.main;
+    if (currentStep === step)
+      return allowSwitchAnytime ? "" : theme.palette.secondary.main;
+    return theme.palette.grey[200];
+  };
+
+  return (
+    <div
+      className={`${styles.step} ${getStep(currentStep, stepIndex)}`}
+      onClick={() =>
+        (allowSwitchAnytime || currentStep >= stepIndex) && setStep(stepIndex)
+      }
+    >
+      <div className={styles.stepContent}>
+        {React.cloneElement(icon as React.ReactElement, {
+          width: "20",
+          color: getStepColor(currentStep, stepIndex),
+        })}
+        <p className={styles.stepText}>{label}</p>
+      </div>
+      {showDoneIcon && currentStep > stepIndex && (
+        <DoneFilledIcon
+          width="16"
+          secondColor={theme.palette.primary.main}
+          color="#FFF"
+        />
+      )}
+    </div>
+  );
+};
+
+export default Step;
--- pages/pfpcollections.tsx
+++ pages/pfpcollections.tsx
@@ -3,91 +3,135 @@ import type { NextPage } from "next";
 import styles from "../styles/pfpcollections.module.css";
 import PfpNftCard from "../components/pfpcollections/pfpNftCard";
 import { ourNfts, NftCollections } from "../utils/constants";
+import Step from "@/components/domains/steps/step";
 
 const PFPCollections: NextPage = () => {
   const [tab, setTab] = useState(0);
+  const allowSwitchAnytime = true;
 
   return (
-    <div className={styles.container}>
-      <div className="grid w-full grid-cols-1 md:grid-cols-[minmax(100px,1fr)_5fr] gap-4">
-        <div className={styles.gallery}>
-          <div className="flex flex-col items-center md:items-start gap-[24px] lg:gap-4">
-            <div className="flex gap-2 lg:hidden">
-              <img src="/icons/avatar.svg" />
-              <span className={`font-bold text-[14px] text-gray-400`}>
-                Your NFTs
-              </span>
-            </div>
-            <div
-              className="flex gap-2 cursor-pointer"
-              onClick={() => setTab(0)}
-            >
+    <div className="w-full flex flex-col xl:flex-row justify-center gap-4 px-3 py-4 lg:px-32 md:px-16 sm:py-12 xl:h-[88vh] mt-[6rem]">
+      <aside className={`${styles.purchaseStepNav}`} role="navigation">
+        <div>
+          <Step
+            stepIndex={0}
+            currentStep={tab}
+            setStep={setTab}
+            icon={
               <img
                 src={`${
                   tab === 0
                     ? "/icons/ecosystem-active.svg"
                     : "/icons/ecosystem-inactive.svg"
                 }`}
+                alt="Starknet ID Ecosystem"
               />
-              <span
-                className={`font-bold text-[14px] ${
-                  tab === 0 ? "" : "text-gray-400"
-                }`}
-              >
-                Starknet ID Ecosystem
-              </span>
-            </div>
-            <div
-              className="flex gap-2 cursor-pointer"
-              onClick={() => setTab(1)}
-            >
+            }
+            label="Starknet ID Ecosystem"
+            showDoneIcon={false}
+            allowSwitchAnytime={allowSwitchAnytime}
+          />
+          <Step
+            stepIndex={1}
+            currentStep={tab}
+            setStep={setTab}
+            icon={
               <img
                 src={`${
                   tab === 1
                     ? "/icons/starknet-active.svg"
                     : "/icons/starknet-inactive.svg"
                 }`}
+                alt="Overall Starknet Ecosystem"
               />
-              <span
-                className={`font-bold text-[14px] ${
-                  tab === 1 ? "" : "text-gray-400"
-                }`}
-              >
-                Overall Starknet Ecosystem
-              </span>
+            }
+            label="Overall Starknet Ecosystem"
+            showDoneIcon={false}
+            allowSwitchAnytime={allowSwitchAnytime}
+          />
+        </div>
+
+        <img
+          src="/visuals/purchaseStepVisual.svg"
+          alt="Domain purchase steps visualization"
+        />
+      </aside>
+
+      <div className={`${styles.purchaseStepNavMobile}`} role="navigation">
+        <Step
+          stepIndex={0}
+          currentStep={tab}
+          setStep={setTab}
+          icon={
+            <img
+              src={`${
+                tab === 0
+                  ? "/icons/ecosystem-active.svg"
+                  : "/icons/ecosystem-inactive.svg"
+              }`}
+              alt="Starknet ID Ecosystem"
+            />
+          }
+          label="Starknet ID Ecosystem"
+          showDoneIcon={false}
+          allowSwitchAnytime={allowSwitchAnytime}
+        />
+        <Step
+          stepIndex={1}
+          currentStep={tab}
+          setStep={setTab}
+          icon={
+            <img
+              src={`${
+                tab === 1
+                  ? "/icons/starknet-active.svg"
+                  : "/icons/starknet-inactive.svg"
+              }`}
+              alt="Overall Starknet Ecosystem"
+            />
+          }
+          label="Overall Starknet Ecosystem"
+          showDoneIcon={false}
+          allowSwitchAnytime={allowSwitchAnytime}
+        />
+
+        <div className="flex justify-center">
+          <img
+            src="/visuals/purchaseStepVisualMobile.svg"
+            alt="Domain purchase steps visualization"
+          />
+        </div>
+      </div>
+
+      <div className="flex-1">
+        {tab === 0 ? (
+          <section>
+            <div className={styles.nfts}>
+              {ourNfts.map((collection, index) => (
+                <PfpNftCard
+                  key={index}
+                  image={collection.imageUri}
+                  name={collection.name}
+                  onClick={() => window.open(collection.infoPage)}
+                />
+              ))}
             </div>
-          </div>
-        </div>
-          <div className="mt-5 lg:mt-12 lg:mx-10 mb-16">
-            {tab === 0 ? (
-              <section>
-                <div className={styles.nfts}>
-                  {ourNfts.map((collection, index) => (
-                    <PfpNftCard
-                      key={index}
-                      image={collection.imageUri}
-                      name={collection.name}
-                      onClick={() => window.open(collection.infoPage)}
-                    />
-                  ))}
-                </div>
-              </section>
-            ) : (
-              <section>
-                <div className={styles.nfts}>
-                  {NftCollections.map((collection, index) => (
-                    <PfpNftCard
-                      key={index}
-                      image={collection.imageUri}
-                      name={collection.name}
-                      onClick={() => window.open(collection.externalLink)}
-                    />
-                  ))}
-                </div>
-              </section>
-            )}
-          </div>
-        </div>
+          </section>
+        ) : (
+          <section>
+            <div className={styles.nfts}>
+              {NftCollections.map((collection, index) => (
+                <PfpNftCard
+                  key={index}
+                  image={collection.imageUri}
+                  name={collection.name}
+                  onClick={() => window.open(collection.externalLink)}
+                />
+              ))}
+            </div>
+          </section>
+        )}
+      </div>
     </div>
   );
 };
