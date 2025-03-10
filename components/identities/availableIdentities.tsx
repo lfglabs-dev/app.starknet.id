6762de9c21c9e23553f982e553cfbd9878d23f71
pedroco3lho <pedrocoelho.nascimento@gmail.com>
2025-03-09 21:54:16 -0300

    style: improve responsiveness

--- components/identities/availableIdentities.tsx
+++ components/identities/availableIdentities.tsx
@@ -152,8 +152,7 @@ const AvailableIdentities = ({ tokenId }: { tokenId: string }) => {
     if (address) {
       // Our Indexer
       fetch(
-        `${
-          process.env.NEXT_PUBLIC_SERVER_LINK
+        `${process.env.NEXT_PUBLIC_SERVER_LINK
         }/addr_to_full_ids?addr=${hexToDecimal(address)}`
       )
         .then((response) => response.json())
@@ -173,52 +172,50 @@ const AvailableIdentities = ({ tokenId }: { tokenId: string }) => {
 
   return (
     <>
-      <div className="min-h-[88vh] overflow-x-visible">
+      <div className="min-h-[88vh] overflow-x-hidden">
         {isIdentityADomain === undefined ? (
           <div>
             <IdentitiesSkeleton />
           </div>
         ) : !isUpdatingPp ? (
           <div
-            className={`${styles.IDScreen} px-[16px] lg:px-0 flex flex-col xl:flex-row justify-center items-center gap-[24px] `}
+            className={`${styles.IDScreen} px-[16px] lg:px-0 flex flex-col xl:flex-row justify-center items-center gap-6 xl:flex-flex-nowrap`}
           >
             <div className=" w-[100%] sm:w-[358px] xl:w-[220px]">
               {ownedIdentities.length !== 1 && (
                 <div
                   className={`
-                         ${styles.sideNav} border w-[100%] md:w-auto h-[319px] xl:h-auto relative flex flex-col items-center justify-between sm:px-[24px]  md:pt-[24px] m-auto shadow-sm rounded-2xl `}
+                         ${styles.sideNav} border w-[100%] md:w-auto h-[319px] xl:h-auto relative flex flex-col items-center justify-between sm:px-[24px] md:pt-[24px] m-auto shadow-sm rounded-2xl `}
                 >
-                  <div className="h-full md:min-h-[80%] w-full flex flex-col gap-[2px] overflow-y-scroll">
+                  <div className="h-full md:min-h-[80%] w-full flex flex-col gap-[2px] hide-scrollbar overflow-y-scroll">
                     {ownedIdentities.map((domain, index) => (
                       <button
                         className={`${
                           domain.id === router.query.tokenId ||
-                          domain.id === tokenId
+                            domain.id === tokenId
                             ? "text-[#402D28]"
                             : " text-[#CDCCCC] hover:text-[#402D28]"
-                        } font-medium text-lg sm:text-md lg:text-lg leading-5 cursor-pointer border-[#4545451A] border-b-[1px] md:border-none md:py-0 py-6 md:my-3 block w-full text-center xl:text-left`}
+                          } font-medium text-lg sm:text-md lg:text-lg leading-5 cursor-pointer border-[#4545451A] border-b-[1px] md:border-none md:py-0 py-6 md:my-3 block w-full text-center xl:text-left`}
                         key={index}
                         onClick={() => router.push(`/identities/${domain.id}`)}
                       >
-                        <div className="flex items-center justify-between w-full">
-                          <div className="truncate max-w-[90%]">
+                        <div className="flex md:flex-row items-center justify-center md:justify-between w-auto md:w-full p-2 text-center">
+                          <div className="truncate max-w-full">
                             {domain.domain ? domain.domain : domain.id}
                           </div>
                           {isDomainExpired(domain.domain_expiry) && (
-                            <>
-                              <div className="group z-50 text-sm">
-                                <a
-                                  data-tooltip-id="my-tooltip"
-                                  data-tooltip-content="Domain Expired"
-                                  data-tooltip-place="right"
-                                  color="secondary"
-                                >
-                                  <div className="w-2 h-2 m-1 bg-red-500 rounded-full">
-                                  </div>
-                                </a>
-                                <Tooltip id="my-tooltip" noArrow={true} style={{ padding: '8px 12px' }} />
-                              </div>
-                            </>
+                            <div className="group z-50 text-sm p-1">
+                              <a
+                                data-tooltip-id="my-tooltip"
+                                data-tooltip-content="Domain Expired"
+                                data-tooltip-place="right"
+                                color="secondary"
+                              >
+                                <div className="w-2 h-2 bg-red-500 rounded-full">
+                                </div>
+                              </a>
+                              <Tooltip id="my-tooltip" noArrow={true} style={{ padding: '8px 12px' }} globalCloseEvents={{ scroll: true }} />
+                            </div>
                           )}
                         </div>
                       </button>
