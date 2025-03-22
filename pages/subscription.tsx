7e916bea860efaed0c39d0413d99bf1b3faafa4a
joel david <daveside00468@gmail.com>
2025-03-22 10:37:31 +0100

    Added 'Cancel Subscription' button with Figma styling

--- pages/subscription.tsx
+++ pages/subscription.tsx
@@ -9,6 +9,11 @@ const AutoRenewalPage: NextPage = () => {
     <div className={homeStyles.screen}>
       <div className={styles.container}>
         <AutoRenewal />
+
+        {/* Cancel Subscription Button */}
+        <div className={styles.cancelContainer}>
+          <button className={styles.cancelButton}>Cancel Subscription</button>
+        </div>
       </div>
     </div>
   );
