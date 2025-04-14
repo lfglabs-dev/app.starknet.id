f83e9fdd02e58d4125da8b73ee5dcdd6ccf89b45
Amiola Demilade <amiolademilade@gmail.com>
2025-04-14 23:09:59 +0100

    fix:#1097  Add a new title when subscription is active

--- components/identities/actions/ActiveSubscriptionCard.tsx
+++ components/identities/actions/ActiveSubscriptionCard.tsx
@@ -2,7 +2,12 @@ import React from "react";
 import styles from "../../../styles/components/identityMenu.module.css";
 
 function ActiveSubscriptionCard() {
-  return <div className={styles.subscriptionSecondary}></div>;
+  return (
+      <div className={styles.subscriptionSecondary}>
+        <h1 className="font-quickZap font-normal text-[14px] text-center py-5 text-[#454545] uppercase">Your Subscription is Active</h1>
+
+      </div>
+  );
 }
 
 export default ActiveSubscriptionCard;
