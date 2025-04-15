2024b4de778c064bd086edc1a66aba3be5a7deaf
Amiola Demilade <amiolademilade@gmail.com>
2025-04-15 11:48:58 +0100

    style: Center subscription active message within a flex container

--- components/identities/actions/ActiveSubscriptionCard.tsx
+++ components/identities/actions/ActiveSubscriptionCard.tsx
@@ -4,7 +4,9 @@ import styles from "../../../styles/components/identityMenu.module.css";
 function ActiveSubscriptionCard() {
   return (
       <div className={styles.subscriptionSecondary}>
-        <h1 className="font-quickZap font-normal text-[14px] text-center py-5 text-[#454545] uppercase">Your Subscription is Active</h1>
+        <div className="flex flex-col items-center justify-center mt-3">
+        <h1 className="font-quickZap font-normal text-[14px] text-center text-[#454545] uppercase">Your Subscription is Active</h1>
+        </div>
 
       </div>
   );
