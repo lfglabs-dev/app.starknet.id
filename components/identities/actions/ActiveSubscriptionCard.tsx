e4f4f818d63ce1ff009574c78171c9cd2ba9f3dd
Amiola Demilade <amiolademilade@gmail.com>
2025-04-17 18:06:05 +0100

    style: Adjust center for active subscription message

--- components/identities/actions/ActiveSubscriptionCard.tsx
+++ components/identities/actions/ActiveSubscriptionCard.tsx
@@ -4,9 +4,12 @@ import styles from "../../../styles/components/identityMenu.module.css";
 function ActiveSubscriptionCard() {
   return (
       <div className={styles.subscriptionSecondary}>
-        <div className="flex flex-col items-center justify-center mt-3">
-        <h1 className="font-quickZap font-normal text-[14px] text-center text-[#454545] uppercase">Your Subscription is Active</h1>
-        </div>
+      <div className="flex flex-col items-center mt-3">
+        <h1 className="font-quickZap font-normal text-[14px] text-center text-[#454545] uppercase">
+          Your Subscription is Active
+        </h1>
+      </div>
+
 
       </div>
   );
