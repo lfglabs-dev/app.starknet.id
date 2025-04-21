ea5ee19d9c291621ce24ff13e62ab8e10ff00cc7
legend4tech <ajuludennis14@gmail.com>
2025-04-21 10:15:55 +0100

    Fix: Identities mobile page - Add margin

--- components/identities/availableIdentities.tsx
+++ components/identities/availableIdentities.tsx
@@ -228,7 +228,7 @@ const AvailableIdentities = ({ tokenId }: { tokenId: string }) => {
               {ownedIdentities.length !== 0 && (
                 <div
                   className={`
-                         ${styles.sideNav} border w-[100%] md:w-auto h-[319px] xl:h-auto relative flex flex-col items-center justify-between sm:px-[24px]  md:pt-[24px] m-auto shadow-sm rounded-2xl mt-[48px] md:mt-0`}
+                         ${styles.sideNav} border w-[100%] md:w-auto h-[319px] xl:h-auto relative flex flex-col items-center md:justify-between sm:px-[24px] md:pt-[24px] m-auto shadow-sm rounded-2xl mt-[16px] md:mt-0`}
                 >
                   <div className="h-full md:min-h-[80%] w-full overflow-y-auto hide-scrollbar flex flex-col gap-[2px] ">
                     {ownedIdentities.map((domain, index) => (
