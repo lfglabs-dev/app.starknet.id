be4e1744de5b59d082e07e3f1b53faf2b9c2b324
legend4tech <ajuludennis14@gmail.com>
2025-04-21 11:59:37 +0100

    Fix: Add the correct favicon for our app

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html>
-      <link rel="icon" href="/visuals/StarknetIdLogo.svg" />
+      <link rel="icon" href="/visuals/favicon.ico" />
       <Head>
         <meta charSet="utf-8" />
         <meta name="theme-color" content="#2CAA6E" />
