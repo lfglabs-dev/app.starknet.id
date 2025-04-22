f18dbff6bf34bb4fa32bcb28892632d41251a2f6
Amiola Demilade <amiolademilade@gmail.com>
2025-04-22 12:19:43 +0100

    Refactor: Simplify ActiveSubscriptionCard layout and styling

--- components/identities/actions/ActiveSubscriptionCard.tsx
+++ components/identities/actions/ActiveSubscriptionCard.tsx
@@ -3,15 +3,13 @@ import styles from "../../../styles/components/identityMenu.module.css";
 
 function ActiveSubscriptionCard() {
   return (
-      <div className={styles.subscriptionSecondary}>
-      <div className="flex flex-col items-center mt-3">
-        <h1 className="font-quickZap font-normal text-[14px] text-center text-[#454545] uppercase">
-          Your Subscription is Active
-        </h1>
-      </div>
+    <div className={`${styles.subscriptionSecondary} flex flex-col items-center justify-start`}>
+    <h1 className="font-quickZap font-normal text-[14px] text-[#454545] uppercase mt-1">
+      Your Subscription is Active
+    </h1>
+  </div>
 
 
-      </div>
   );
 }
 
