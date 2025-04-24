9ef3f56c5964efa6577d690f466ff1178dc95ba4
Nico <60229704+Marchand-Nicolas@users.noreply.github.com>
2025-04-24 17:18:18 +0100

    fixing margin

--- components/identities/actions/ActiveSubscriptionCard.tsx
+++ components/identities/actions/ActiveSubscriptionCard.tsx
@@ -4,12 +4,10 @@ import styles from "../../../styles/components/identityMenu.module.css";
 function ActiveSubscriptionCard() {
   return (
     <div className={`${styles.subscriptionSecondary} text-center`}>
-    <h1 className="font-quickZap font-normal text-[14px] text-[#454545] uppercase mt-1">
-      Your Subscription is Active
-    </h1>
-  </div>
-
-
+      <h1 className="font-quickZap font-normal text-[14px] text-[#454545] uppercase mt-[6px]">
+        Your Subscription is Active
+      </h1>
+    </div>
   );
 }
 
