00cb2fcbbcebe5de627087c990c3da7e4c29b07d
Amiola Demilade <amiolademilade@gmail.com>
2025-04-24 17:10:03 +0100

    Refactor: Update ActiveSubscriptionCard layout and adjust flex direction in CSS

--- components/identities/actions/ActiveSubscriptionCard.tsx
+++ components/identities/actions/ActiveSubscriptionCard.tsx
@@ -3,7 +3,7 @@ import styles from "../../../styles/components/identityMenu.module.css";
 
 function ActiveSubscriptionCard() {
   return (
-    <div className={`${styles.subscriptionSecondary} flex flex-col items-center justify-start`}>
+    <div className={`${styles.subscriptionSecondary} text-center`}>
     <h1 className="font-quickZap font-normal text-[14px] text-[#454545] uppercase mt-1">
       Your Subscription is Active
     </h1>
