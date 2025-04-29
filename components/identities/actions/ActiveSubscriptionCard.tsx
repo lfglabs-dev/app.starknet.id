a2323e362f3263f1cb6cabeb3a8294c2c6e49625
Nico <60229704+Marchand-Nicolas@users.noreply.github.com>
2025-04-29 12:15:28 +0200

    fix call

--- components/identities/actions/ActiveSubscriptionCard.tsx
+++ components/identities/actions/ActiveSubscriptionCard.tsx
@@ -26,7 +26,7 @@ function ActiveSubscriptionCard({
         type="button"
         style={{ backgroundColor: "#cd3a31" }}
         className="font-quickZap font-normal text-[12px] text-[#ffffff] p-[10px] rounded-lg uppercase mt-[6px]"
-        onClick={disableRenewal}
+        onClick={() => disableRenewal()}
       >
         CANCEL SUBSCRIPTION
       </button>
