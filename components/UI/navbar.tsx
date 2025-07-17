11394f696401dbf5addb2b61a53982872603c717
Verifieddanny <dannyclassic56@gmail.com>
2025-07-17 16:28:06 +0100

    Fix: Build Error

--- components/UI/navbar.tsx
+++ components/UI/navbar.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import React from "react";
 import Link from "next/link"
 import { useState, useEffect, type FunctionComponent, useContext } from "react"
 import { AiOutlineMenu } from "react-icons/ai"
